e147aa6e28813b99a1499559408a577666da2ef5
Oguz Kolukisa <okolukisa@gmail.com>
2026-01-11 00:40:08 +0300

    .

--- install_scripts/anaconda.sh
+++ install_scripts/anaconda.sh
@@ -2,23 +2,18 @@
 
 set -e
 
-if command -v conda &>/dev/null; then
-  printf "Anaconda already installed.\n"
-  exit 0
-fi
-
-printf "Downloading Anaconda installer..."
+printf "Downloading Anaconda installer...\r"
 curl -fsSL https://repo.anaconda.com/archive/Anaconda3-2025.12-1-Linux-x86_64.sh -o anaconda.sh
-printf "\rDownloading Anaconda installer... Done\n"
+printf "\rDownloading Anaconda installer... Done\r"
 
-printf "Installing Anaconda..."
+printf "Installing Anaconda...\r"
 bash anaconda.sh -b -p $HOME/anaconda3
-printf "\rInstalling Anaconda... Done\n"
+printf "Installing Anaconda... Done\r"
 
 # Initialize conda for bash
 $HOME/anaconda3/bin/conda init bash
 
 # Remove installer after installation
 rm -f anaconda.sh
-printf "Removed Anaconda installer.\n"
-printf "Anaconda installed. Please restart your terminal or run 'source ~/.bashrc' to activate conda.\n"
+printf "Removed Anaconda installer.\r"
+printf "Anaconda installed. Please restart your terminal or run 'source ~/.bashrc' to activate conda.\r"
